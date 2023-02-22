738db1839ed6f01801c594f8b8a50eafc74cbcdf
adrien.dacostaveiga.etu <adrien.dacostaveiga.etu@univ-lille.fr>
2023-02-22 11:25:29 +0100

    Commande : Controler commande vierge, à implémenter

---
+++ WEB-INF/src/controlers/CommandeRestApi.java
@@ -0,0 +1,20 @@
+package controlers;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CommandeRestApi extends HttpServlet{
+    
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        // TO DO
+    }
+
+    public void doPost (HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        // TO DO
+    }
+
+}
